aae20d134dc5daef8037eeb4cb05d721246e507c
Meer Uzair <meer.uxair007@gmail.com>
2024-01-21 20:20:38 +0530

    Environment Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Hello world!</h1>
-    </main>
-  );
+    return (
+        <main className="flex h-screen items-center justify-center">
+            <h1>Hello world!</h1>
+        </main>
+    )
 }
