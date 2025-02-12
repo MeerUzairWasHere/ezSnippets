095a28bda6d0ef7bf6cdab6bb844e27733387f25
Meer Uzair <meer.uxair007@gmail.com>
2025-02-13 00:48:47 +0530

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -9,16 +9,17 @@ export type SnippetType = {
     createdAt: Date
     language: string
     highlightedLines?: string[]
+    tabs: TabType[]
+}
+export type TabType = {
+    name: string
+    code: string
+    language: string
 }
 
-export type CreateAndEditSnippetType = z.infer<
-    typeof createAndEditSnippetSchema
->
-// Define the Tag schema using Zod
-
-export const createAndEditSnippetSchema = z.object({
-    title: z.string().min(2, {
-        message: 'Title must be at least 2 characters.',
+const TabSchema = z.object({
+    name: z.string().min(1, {
+        message: 'Tab name is required.',
     }),
     code: z.string().min(2, {
         message: 'Code must be at least 2 characters.',
@@ -26,7 +27,20 @@ export const createAndEditSnippetSchema = z.object({
     language: z.string().min(1, {
         message: 'Must be a valid language.',
     }),
+})
+
+export const createAndEditSnippetSchema = z.object({
+    title: z.string().min(2, {
+        message: 'Title must be at least 2 characters.',
+    }),
     highlightedLines: z
         .union([z.string(), z.array(z.string())])
-        .transform((val) => (Array.isArray(val) ? val : [val])), // Ensure it's always an array of strings
+        .transform((val) => (Array.isArray(val) ? val : [val])),
+    tabs: z.array(TabSchema).min(1, {
+        message: 'At least one tab is required.',
+    }),
 })
+
+export type CreateAndEditSnippetType = z.infer<
+    typeof createAndEditSnippetSchema
+>
