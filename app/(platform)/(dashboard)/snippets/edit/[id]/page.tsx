56d5611132a2dcc894550a916ca685525cb71998
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 20:40:06 +0530

    Update page.tsx

--- app/(platform)/(dashboard)/snippets/edit/[id]/page.tsx
+++ app/(platform)/(dashboard)/snippets/edit/[id]/page.tsx
@@ -1,24 +1,11 @@
-//start
 import { getSingleSnippetsAction } from '@/actions/snippet.actions'
-import {
-    dehydrate,
-    HydrationBoundary,
-    QueryClient,
-} from '@tanstack/react-query'
-import { EditSnippetForm } from '../../../_components/EditSnippetForm'
+import EditSnippetForm from '../../../_components/EditSnippetForm'
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const queryClient = new QueryClient()
-
-    await queryClient.prefetchQuery({
-        queryKey: ['snippet', params.id],
-        queryFn: () => getSingleSnippetsAction(params.id),
-    })
+    const snippet = getSingleSnippetsAction(params.id)
     return (
         <main className="w-full md:max-w-screen-sm">
-            <HydrationBoundary state={dehydrate(queryClient)}>
-                <EditSnippetForm snippetId={params.id} />
-            </HydrationBoundary>
+            <EditSnippetForm id={params.id} />
         </main>
     )
 }
