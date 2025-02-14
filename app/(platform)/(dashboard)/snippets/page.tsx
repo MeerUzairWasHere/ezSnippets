dc6cd294bf392c193c89aebb0fb7ffcd0ceea273
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 17:08:33 +0530

    Update page.tsx

--- app/(platform)/(dashboard)/snippets/page.tsx
+++ app/(platform)/(dashboard)/snippets/page.tsx
@@ -1,24 +1,6 @@
-import {
-    dehydrate,
-    HydrationBoundary,
-    QueryClient,
-} from '@tanstack/react-query'
-
 import { SnippetsList } from '../_components/SnippetsList'
-import { getAllSnippetsAction } from '@/actions/snippet.actions'
-
-
-const queryClient = new QueryClient()
 
 async function SnippetsPage() {
-    await queryClient.prefetchQuery({
-        queryKey: ['snippets'],
-        queryFn: () => getAllSnippetsAction(),
-    })
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <SnippetsList />
-        </HydrationBoundary>
-    )
+    return <SnippetsList />
 }
 export default SnippetsPage
