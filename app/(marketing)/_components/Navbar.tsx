a936367972ad14c46a14d10c7de9586a38eaf11c
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 20:40:02 +0530

    Update Navbar.tsx

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
     return (
         <header className="fixed top-0 flex h-14 w-full items-center border-b bg-white px-4 shadow-sm">
             <div className="mx-auto flex w-full items-center justify-between md:max-w-screen-2xl">
-                <Logo />
+                <Logo inHomePage={true} />
                 <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
                     <Button size="sm" variant="outline" asChild>
                         <Link href="/sign-in">Login</Link>
