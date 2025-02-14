5e07c42ddf04f6bd4c9fa88a7badc06a6f90b0f5
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 20:40:09 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-
-
-export const handleError = (error: unknown) => {
-  console.error(error)
-  throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
 }
