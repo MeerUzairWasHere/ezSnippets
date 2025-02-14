61851254b488d465d95911413a915a7f44c9b38d
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 20:40:05 +0530

    Update layout.tsx

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -10,14 +10,14 @@ export default function DashboardLayout({
         <div className="h-full">
             <Navbar />
             <section className="w-full px-4 pt-20 md:pt-24">
-                <div className="flex gap-x-7">
+                <div className="flex">
                     {/* Fixed Sidebar */}
                     <div className="fixed left-0 top-24 hidden h-[calc(100%-6rem)] w-64 shrink-0 md:block">
                         <Sidebar />
                     </div>
 
                     {/* Main Content with left margin to avoid overlap */}
-                    <div className="w-full md:ml-72">{children}</div>
+                    <div className="w-full md:ml-60">{children}</div>
                 </div>
             </section>
         </div>
