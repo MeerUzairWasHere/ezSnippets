ec66b182d8f417fc9af37fcf85a3821d1245d80d
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 20:40:06 +0530

    Update page.tsx

--- app/(platform)/(dashboard)/snippets/[id]/page.tsx
+++ app/(platform)/(dashboard)/snippets/[id]/page.tsx
@@ -1,24 +1,7 @@
 //start
-import { getSingleSnippetsAction } from '@/actions/snippet.actions'
-import {
-    dehydrate,
-    HydrationBoundary,
-    QueryClient,
-} from '@tanstack/react-query'
-import { SnippetInfo } from '../../_components/SnippetInfo'
+
+import SnippetInfo from '../../_components/SnippetInfo'
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const queryClient = new QueryClient()
-
-    await queryClient.prefetchQuery({
-        queryKey: ['snippet', params.id],
-        queryFn: () => getSingleSnippetsAction(params.id),
-    })
-    return (
-        <main className="w-full  ">
-            <HydrationBoundary state={dehydrate(queryClient)}>
-                <SnippetInfo id={params.id} />
-            </HydrationBoundary>
-        </main>
-    )
+    return <SnippetInfo snippetId={params.id} />
 }
