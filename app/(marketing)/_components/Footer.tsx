ce2a03ee7278fcd7e8de381659fd6daf6c38b28f
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 20:40:02 +0530

    Update Footer.tsx

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     return (
         <footer className="fixed bottom-0 w-full border-t bg-slate-100 p-3 ">
             <div className="mx-auto flex w-full items-center justify-between md:max-w-screen-2xl">
-                <Logo />
+                <Logo inHomePage={true} />
                 <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
                     <Button size="sm" variant="ghost">
                         Privacy Policy
