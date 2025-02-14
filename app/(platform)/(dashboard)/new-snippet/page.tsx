f3310d9bdece0993590622b166b187d23c8b8702
Meer Uzair <meer.uxair007@gmail.com>
2025-02-14 17:08:32 +0530

    Update page.tsx

--- app/(platform)/(dashboard)/new-snippet/page.tsx
+++ app/(platform)/(dashboard)/new-snippet/page.tsx
@@ -1,8 +1,6 @@
- 
 import CreateSnippetForm from '../_components/CreateSnippetForm'
- 
+
 export default async function Page() {
-    
     return (
         <main className="w-full md:max-w-screen-sm">
             <CreateSnippetForm />
